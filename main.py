477282024ab5f5bd7bd424e6c8dc4aec5320afa3
dinkagery <dinkagery@mail.ru>
2024-11-04 10:25:16 +0300

    1 коммит

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-#тестирую коммиты
+#тестирую коммитыbb
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+#qwe
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
